07f51a34a64136c9ad1557ad3c4015e80969fa6e
Nils svenungsson <Nilssvenungsson@gmail.com>
2024-01-18 10:48:21 +0100

    nytt

--- 7101-projekt blackjack/spel.py
+++ 7101-projekt blackjack/spel.py
@@ -2,27 +2,40 @@ import random
 
 kortlek = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
 
-spelare = []
+player = []
 
 dealer = []
+
+choice = "hit"
 
 random.shuffle(kortlek)
 
 
 def start():
-    spelare.append(kortlek[0])
+    player.append(kortlek[0])
     kortlek.pop(0)
     dealer.append(kortlek[0])
     kortlek.pop(0)
-    spelare.append(kortlek[0])
+    player.append(kortlek[0])
     kortlek.pop(0)
     dealer.append(kortlek[0])
+    kortlek.pop(0)
+    print(player[0], player[1])
+    print(dealer[0], "x")
+
+def chose():
+    while choice.lower() == "hit":
+        choice = input("hit or stay:")
+        if choice.lower() == "hit":
+            player.append(kortlek[0])
+            kortlek.pop(0)
+            print(player[0], player[1], player[2])
+        elif choice.lower() == "stay":
+            print(sum(player))
+        elif sum(player) < 21:
+            print("bust")
 
 
 start()
 
-print(kortlek)
-
-print(spelare)
-
-print(dealer[0], "x")
+chose()
